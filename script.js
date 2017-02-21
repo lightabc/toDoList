d727aaf538d79237a18ef55ea1a79e095c9c30f3
lcq <lcqfront@163.com>
2017-02-21 16:30:30 +0800

    在标记任务为已完成后，在localstorage中也标记

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ var input = document.getElementsByTagName("input")[0];
 //添加任务
 input.onkeydown = function (e) {
     if (e.keyCode == 13) {
+        var inputText = input.value;
         //如果内容为空，不允许添加任务
-        if (!input.value) {
+        if (!inputText) {
             return false
         }
-        //todo 相关信息输入到localstorage
-        localStorage.setItem(input.value, "0");
+        //相关信息输入到localstorage
+        localStorage.setItem(inputText, "0");
         //根据localstorage中的信息刷新view
         //如果按下了回车，就添加一个任务
         var newItem = document.createElement("div");
@@ -15,10 +16,13 @@ input.onkeydown = function (e) {
         //新任务的点击事件
         newItem.onclick = function () {
             this.style.opacity = 0.5;
-            //todo 在localstorage中标记为已完成
-            //todo 在localstorage中查找这个任务，标记value=0
+            //在localstorage中标记为已完成
+            //在localstorage中查找这个任务，标记value=0
+            if(localStorage.getItem(inputText)){
+                localStorage.setItem(inputText,"1")
+            }
         };
-        newItem.innerHTML = input.value;
+        newItem.innerHTML = inputText;
         var targetElement = document.getElementsByClassName("item")[0];
         if (!targetElement) {
             document.body.appendChild(newItem);
