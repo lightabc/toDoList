ca0b0d86835b7673b1832a9d0380c473946007b0
lcq <lcqfront@163.com>
2017-02-25 15:15:40 +0800

    增加checkbox，可以更新数据到数组

--- script.js
+++ script.js
@@ -11,19 +11,18 @@ app.controller("ctrl01", function ($scope) {
     $scope.add = function (inputText) {
         $scope.list.push({text: inputText, done: false});
     };
-    $scope.changeStatus = function (element) {
+    //任务状态
+/*    $scope.changeStatus = function (element) {
         var text = element.innerHTML;
         for (var i = 0; i < $scope.list.length; i++) {
             if ($scope.list[i].text == text) {
                 var status = $scope.list[i].done;
                 if (status == true) {
                     $scope.list[i].done = false;
-                    element.style.opacity = 1;
                 } else {
                     $scope.list[i].done = true;
-                    element.style.opacity = 0.5;
                 }
             }
         }
-    }
+    }*/
 });
