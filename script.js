d301e2af14276a7e5297077c0897d4cbd761beb4
lcq <lcqfront@163.com>
2017-02-25 15:21:23 +0800

    checkbox

--- script.js
+++ script.js
@@ -11,18 +11,4 @@ app.controller("ctrl01", function ($scope) {
     $scope.add = function (inputText) {
         $scope.list.push({text: inputText, done: false});
     };
-    //任务状态
-/*    $scope.changeStatus = function (element) {
-        var text = element.innerHTML;
-        for (var i = 0; i < $scope.list.length; i++) {
-            if ($scope.list[i].text == text) {
-                var status = $scope.list[i].done;
-                if (status == true) {
-                    $scope.list[i].done = false;
-                } else {
-                    $scope.list[i].done = true;
-                }
-            }
-        }
-    }*/
 });
