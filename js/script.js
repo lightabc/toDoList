d4993e4b3799220b5ec620417e73c89883622c96
lcq <lcqfront@163.com>
2017-02-28 22:12:31 +0800

    保存数据到localstorage

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-console.log(store);
 var app = angular.module('myapp', []);
 app.controller("ctrl01", function ($scope) {
-    $scope.inputText = "";
-    $scope.list = [{text: "clean house", done: false}];
+    $scope.list = [];
+    //恢复数据
+    console.log(store.get("list"));
     $scope.checkInput = function ($event) {
         if ($event.keyCode == 13) {
             $scope.add($scope.inputText);
             $scope.inputText = "";
         }
     };
-    $scope.add = function (inputText) {
-        $scope.list.unshift({text: inputText, done: false});
+    $scope.add = function () {
+        $scope.list.unshift({text: $scope.inputText, done: false});
+        store.set("list", $scope.list);
     };
 });
