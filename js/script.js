41067e819c1268937f45838b9350c7042eed8279
lcq <lcqfront@163.com>
2017-03-01 16:18:18 +0800

    数据恢复，在重新打开页面时

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 var app = angular.module('myapp', []);
 app.controller("ctrl01", function ($scope) {
+    $scope.inputText = "";
     $scope.list = [];
-    //恢复数据
-    console.log(store.get("list"));
+    var data = store.get("list");
+    if (data) {
+        console.log(data);
+        $scope.list = data;
+    } else {
+        $scope.list = [];
+    }
+    //添加任务
     $scope.checkInput = function ($event) {
+        if ($scope.inputText == "") {
+            return false
+        }
         if ($event.keyCode == 13) {
             $scope.add($scope.inputText);
             $scope.inputText = "";
@@ -13,4 +23,5 @@ app.controller("ctrl01", function ($scope) {
         $scope.list.unshift({text: $scope.inputText, done: false});
         store.set("list", $scope.list);
     };
+    //todo 恢复数据
 });
